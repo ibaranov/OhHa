3c74e161723df640da148291406f7dcaa4b7fcf7
ibaranov <ivan.baranov@aalto.fi>
2013-08-01 16:42:00 +0300

    Lisaa

--- DoodleJump/src/doodlejump/DoodleJump.java
+++ DoodleJump/src/doodlejump/DoodleJump.java
@@ -1,8 +1,19 @@
 
 package doodlejump;
 
+/**
+ * 
+ * @author Ivan
+ * Documentation start
+ */
 public class DoodleJump {
-
+    
+    /**
+     * 
+     * @param args
+     * this is the main class
+     */
+    
     public static void main(String[] args) {
         System.out.println("Testi");
     }
